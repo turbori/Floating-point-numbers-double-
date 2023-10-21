fd8125d3b6217b1350472bcb4fbc902ad2dfe09e
turbori <132469797+turbori@users.noreply.github.com>
2023-10-21 00:09:59 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static void main(String[] args) {
+        double c, h;
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("What is your C value? ");
+        c = scnr.nextDouble();
+
+        h = -2.0 * (c * c) + (7.0 * c) + 7.5;
+
+        System.out.printf("%.3f\n", h);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+    }
